42562bdd201225fb805f6e6b5109968044b557bd
afrid.md <122868488+Afrid-Md@users.noreply.github.com>
2023-06-03 15:39:21 +0530

    profile update Page added

---
+++ profilePage/profile-page.js
@@ -0,0 +1,105 @@
+import { useRef, useContext, useState } from "react";
+import "./profile-page.css";
+import { Button, Form } from "react-bootstrap";
+import AuthContext from "../../auth/auth-context/auth-context";
+import { useHistory } from "react-router-dom";
+
+function ProfilePage() {
+  const history = useHistory();
+  const [err, setErr] = useState(false);
+  const [urlErr, setUrlErr] = useState(false);
+  const [profileUpdated, setProfileUpdated] = useState(false);
+  const authCtx = useContext(AuthContext);
+  const nameInputRef = useRef();
+  const photoUrlRef = useRef();
+
+  const updateProfileHandler = () => {
+    const enteredName = nameInputRef.current.value;
+    const enteredPhotoUrl = photoUrlRef.current.value.trim();
+
+    if (enteredName.trim().length < 5) {
+      console.log("checking");
+      setErr(true);
+      setTimeout(() => {
+        setErr(false);
+      }, 3000);
+      return;
+    } else if (!enteredPhotoUrl.includes("http")) {
+      setUrlErr(true);
+      setTimeout(() => {
+        setUrlErr(false);
+      },3000);
+      return;
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyA3AbBTqHOLSTMDbMunfXa_oG8FAq8PlX4",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: authCtx.token,
+            displayName: enteredName,
+            photoUrl: enteredPhotoUrl,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.ok) {
+            setProfileUpdated(true);
+            setTimeout(() => {
+              setProfileUpdated(false);
+            }, 3000);
+            return res.json();
+          } else {
+            return res.json().then((data) => {
+              let newError = data.error.message;
+              throw new Error(newError);
+            });
+          }
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+      nameInputRef.current.value = null;
+      photoUrlRef.current.value = null;
+    }
+  };
+
+  const cancelHandler = () => {
+    history.replace("/homePage");
+  };
+
+  return (
+    <div>
+      <div className="profilePage">
+        <h1 className="contact">Contact details</h1>
+        <Button variant="outline-danger" onClick={cancelHandler}>
+          cancel
+        </Button>
+      </div>
+      <div className="profileForm">
+        <Form>
+          <label className="labell">Full Name:</label>
+          <input type="text" ref={nameInputRef} />
+          <label className="labell">Profile Photo Url:</label>
+          <input type="url" ref={photoUrlRef} />
+        </Form>
+      </div>
+      {profileUpdated && (
+        <p className="profileUpdated">Your profile has been updated!</p>
+      )}
+      {err && <p className="error01">Name must be greater than 5</p>}
+      {urlErr && <p className="error01">enter valid Url!</p>}
+      <div className="updateButton">
+        <Button onClick={updateProfileHandler}>Update</Button>
+      </div>
+    </div>
+  );
+}
+export default ProfilePage;
